c6369d68e2efb82375818e560b129937ccccda25
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-01-15 22:06:11 +0200

    final

--- lists_as_stacks_and_queues/stacked_queries324.py
+++ lists_as_stacks_and_queues/stacked_queries324.py
@@ -4,11 +4,11 @@ my_int = int(input())
 map_functions = {
     1: lambda x: stack.append(x[1]),
     2: lambda x: stack.pop() if stack else None,
-    3: lambda x: print(max(stack)),
-    4: lambda x: print(min(stack)),
+    3: lambda x: print(max(stack)) if stack else None,
+    4: lambda x: print(min(stack)) if stack else None,
 }
 for i in range(my_int):
     query = list(map(int, input().split()))
     map_functions[query[0]](query)
 stack.reverse()
-print(', '.join(str(s) for s in stack))
+print(*stack, sep=', ')
