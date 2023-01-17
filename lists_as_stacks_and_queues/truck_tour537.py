25280c99ba6b333d2f562af442405ec1651c07b5
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-01-17 13:00:02 +0200

    with notes

--- lists_as_stacks_and_queues/truck_tour537.py
+++ lists_as_stacks_and_queues/truck_tour537.py
@@ -9,12 +9,14 @@ for pump in range(0, pumps):
 deque_copy = petrol_data.copy()
 
 while deque_copy:
+    '''pop the first element of the copy so that if the correct condition is fulfilled, it will keep popping
+    until the list is empty. Otherwise the copied list will be renewed to be like the full, modified original list'''
     gas, distance = deque_copy.popleft()
     tank += gas
 
     if tank < distance:
         petrol_data.append(petrol_data.popleft())
-        deque_copy = petrol_data.copy()
+        deque_copy = petrol_data.copy()  # reset the copied list to be like the modified original list so we can then pop the new rotated value
         index += 1
         tank = 0
     else:
