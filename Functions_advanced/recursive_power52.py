2603024714c0e8054a748df47deb9130b7f03855
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-02-08 13:06:02 +0200

    with recursion

--- Functions_advanced/recursive_power52.py
+++ Functions_advanced/recursive_power52.py
@@ -1,5 +1,10 @@
 def recursive_power(number, power):
-    return number ** power
+    if power == 0:
+        return 1
+    elif power == 1:
+        return number
+
+    return number * recursive_power(number, power - 1)
 
 
 
